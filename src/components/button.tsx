e307685f29498b5cf9bc0a7c22d6e5a1294894aa
iamBevan <iambevan@gmail.com>
2021-07-04 04:09:21 +0100

    initial commit 🐒

---
+++ src/components/button.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import styles from './button.module.scss'
+
+const Button = (): JSX.Element => {
+	return <button className={styles.button}>Button</button>
+}
+export { Button }
